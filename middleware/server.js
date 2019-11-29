d897dabd7a43248e2ebd997dad1ad8a0f3ad9a49
rajmvjn <rajmvjn@gmail.com>
2019-11-29 08:16:15 +0530

    added more dependencies and script changes

--- middleware/server.js
+++ middleware/server.js
@@ -7,7 +7,7 @@ app.use('/api/auction', require('./routes/api/auction/auction'));
 app.use('/api/auction/finalize-auction', require('./routes/api/auction/auction'));
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
